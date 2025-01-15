f265f695064f4ec83a06534b14fc6c1341176623
Prado <jv_prado@outlook.com>
2025-01-15 19:17:34 -0300

    Adiciona o contador de tentativas

--- app.js
+++ app.js
@@ -2,18 +2,30 @@ alert("Bem-vindo ao jogo do número secreto!");
 
 let numeroSecreto = 29;
 console.log("O número secreto é: " + numeroSecreto);
-let chute
+let chute;
+let tentativas = 1;
+
 while (numeroSecreto != chute) {
     chute = prompt("Digite um número entre 1 e 30");
 
-
-if (numeroSecreto == chute) {
-    alert(`Parabéns, você acertou! O número secreto é ${numeroSecreto}!`);
-} else {    
-    if (numeroSecreto < chute) {
-        alert(`O numero secreto é MENOR que ${chute}`);
-    } else {
-        alert(`O numero secreto é MAIOR que ${chute}`);
+    if (numeroSecreto == chute) {
+        alert(
+            `Parabéns, você acertou!\n`+
+            `O número secreto é: ${numeroSecreto}\n`+
+            `Você acertou em: ${tentativas} tentativas!`
+        );
+    } else {    
+        if (numeroSecreto < chute) {
+            alert(
+                `O numero secreto é MENOR que ${chute}\n`+
+                `Tentativas: ${tentativas}`
+            );
+        } else {
+            alert(
+                `O numero secreto é MAIOR que ${chute}\n`+
+                `Tentativas: ${tentativas}`
+            );
+        }
     }
-}
+    tentativas++;
 }
